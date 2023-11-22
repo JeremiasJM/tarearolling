e99df8dab56b6c3283cd0f07850ae0bb5228fbbd
georginacostilla <georginacostilla.gc@gmail.com>
2023-11-22 20:48:32 -0300

    agrego codigo para eliminar producto

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-/* Hola */
+// Trabajo en equipo
+// Carrito de productos 
+// Carrito de compras con Array
+// ■ Crear una aplicación de carrito de compras donde se ingresen
+// productos como elementos en un array.
+// ■ las tareas que debe hacer son las siguientes:
+// ● Agregar producto al carrito
+// ● Listar los productos del carrito
+// ● Buscar un producto en el carrito
+// ● Filtrar productos que coincidan con una palabra, parte de ella
+// o una letra
+// ● Eliminar producto del carrito
+
+/* crea un arrar de compras */
+
+let carritoCompras = ["manzana","pera","anana","frutilla"];
+console.log(carritoCompras)
+/* Agregar producto al carrito con funciones */
+
+function agregarProducto(producto){
+    let resultado = carritoCompras.push(producto);
+    return resultado;
+}
+agregarProducto("uva"); 
+console.log("Agregamos Uva", carritoCompras)
+
+//Eliminar producto del carrito
+delete carritoCompras [0]
+console.log(carritoCompras)
