e5f30e4f7767f68405960f1d9dc122e44d7d8d54
Axel Molina <molinaaxel.agustin1@gmail.com>
2023-11-22 20:26:41 -0300

    Funcion

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 /* Hola */
+
+
+let carritoDeCompras = ["manzana", "pera", "anana", "frutilla","palta"];
+console.log(carritoDeCompras);
+
+
+
+function listarCarrito(){
+console.log("Productos en el carrito: ");
+for(let i= 0; i<carritoDeCompras.length;i++){
+    console.log(carritoDeCompras);
+}
+
+}
+
+listarCarrito();
